e8ef8261fcc2cd66bac6381c29768958a01062dc
Web-UI-Developer <sampreet.github@gmail.com>
2020-08-10 00:13:48 -0400

    class components

---
+++ src/component/page/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About Page</h1>
+      <h3> Find Github user profiles </h3>
+      <h5>Version : 1.00.00</h5>
+    </Fragment>
+  );
+};
+
+export default About;
